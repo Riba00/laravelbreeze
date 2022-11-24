cb056855b62aa9ad48fe72ce5de8d89c595a7160
Riba00 <jordiriba@iesebre.com>
2022-11-24 16:29:59 +0100

    Laravel middleware acabat

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,24 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['autenticacio'])->group(function (){
+    Route::get('/privada1',function (){
+        echo 'Privada1';
+    });
+    Route::get('/privada2',function (){
+        echo 'Privada2';
+    });
+});
+
+//Route::get('/privada1',function (){
+//
+//    echo '/privada1';
+//})->middleware(['autenticacio']);
+//
+//Route::get('/privada2',function (){
+//    echo '/privada2';
+//
+//})->middleware(['autenticacio']);
 
 require __DIR__.'/auth.php';
